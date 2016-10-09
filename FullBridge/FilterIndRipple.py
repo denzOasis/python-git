d15cb4f5841886022d2d6f5bb48faf972cdbdc81
denzOasis <pat89@gmx.at>
2016-10-09 21:20:32 +0200

    added HCA

--- FullBridge/FilterIndRipple.py
+++ FullBridge/FilterIndRipple.py
@@ -154,6 +154,37 @@ Vdc = 2*38
 L = 3e-6
 C = 3.3e-6*6
 
+# inductor current ripple is max. at Da = Db = 0.5
+D = 0.5
+Va = Vdc*pwm(t, D, fs, centeralign = True, shift=0) - Vdc/2
+Vb = Vdc*pwm(t, D, fs, centeralign = True, shift=120) - Vdc/2
+Vc = Vdc*pwm(t, D, fs, centeralign = True, shift=240) - Vdc/2
+
+Ia = calcripple(t, Va)/L
+Ib = calcripple(t, Vb)/L
+Ic = calcripple(t, Vc)/L
+
+# plot
+fig = plt.figure(figsize=(8, 6), dpi=80)
+fig.suptitle('HCA-CM max. inductor current ripple')
+ax = fig.add_subplot(2, 1, 1)
+ax.plot(t, Va)
+ax.plot(t, Vb)
+ax.plot(t, Vc)
+ax.set_ylim(createLimits(.1, Va))
+ax.set_ylabel('voltage [V]')
+ax.grid(True)
+ax = fig.add_subplot(2, 1, 2)
+ax.plot(t, Ia)
+ax.plot(t, Ib)
+ax.plot(t, Ic)
+ax.set_ylabel('current [A]')
+annotate_ripple(ax, t, Ia, 0)
+ax.set_xlabel('time [sec]')
+ax.grid(True)
+plt.show()
+fig.savefig('HCA_CM_L_ripple.pdf')
+
 # capacitor voltage ripple is max. at D = 1/2 or 1/6 or 5/6
 # assuming that the total ripple flows over the capacitor
 D = 1/2
